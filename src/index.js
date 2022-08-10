8b6dabfa1236944a32b71a7f095709636d7d8237
Valentin Saulyak <vsaulyak66@gmail.com>
2022-08-10 12:22:25 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  
+  for(let i = 0; i < str.length; i++) {
+    let symbol = str[i];
+    
+    for(let i = 0; i < bracketsConfig.length; i++) {
+      let config = bracketsConfig[i];
+      let topSymbol = stack[stack.length - 1];
+      if(config.includes(symbol)) {
+        if (symbol === config[1]) {
+          if (topSymbol === config[0]) {
+            stack.pop();
+          } else {
+            stack.push(symbol);
+          }
+        } else {
+          stack.push(symbol);
+        }
+      }
+    }
+  }
+  return stack.length === 0;
 }
